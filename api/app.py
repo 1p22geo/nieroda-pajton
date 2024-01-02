013b708f0a5e670d9b0fa8f6cdadf2a1eea33b0d
Geodecki 2P gr.1 <113300767+1p22geo@users.noreply.github.com>
2024-01-02 09:31:16 +0000

    sorting

--- api/app.py
+++ api/app.py
@@ -1,10 +1,20 @@
 from flask import Flask, request
 import json
+import sort
 from database import Database
 
 app = Flask("app")
 
 data = Database()
+
+@app.route('/sorted/<ix>', methods=['GET'])
+def sorted_route(ix): 
+    fn = lambda x: x[ix]
+    return (json.dumps(
+        {
+            "data": sort.fnsort(data.get(), fn)
+        }
+    ))
 
 @app.route('/data', methods=['POST', 'GET'])
 def data_route(): 
---
+++ api/sort.py
@@ -0,0 +1,25 @@
+def fnsort(data, fun):
+    if len(data)==0:
+        return []
+    if len(data)==1:
+        return data
+    if len(data)==2:
+        if fun(data[0]) > fun(data[1]):
+            return [data[1], data[0]]
+        else:
+            return [data[0], data[1]]
+    pivot = data[0]
+    ar1 = []
+    ar2 = []
+    for elem in data[1:]:
+        if fun(elem)>fun(pivot):
+            ar2.append(elem)
+        else:
+            ar1.append(elem)
+    
+    arr = []
+    arr.extend(fnsort(ar1, fun))
+    arr.append(pivot)
+    arr.extend(fnsort(ar2, fun))
+
+    return arr
