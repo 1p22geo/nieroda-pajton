8fd560173ce826a396829c79a28a6c6aa497049a
1p22geo <1p22geodecki@gmail.com>
2024-06-11 14:32:44 +0200

    patch(osiedle): fixed the white house

--- osiedle/components/whiteHouse.py
+++ osiedle/components/whiteHouse.py
@@ -1,11 +1,11 @@
 import lib.move
 
 
-def whiteHouse(t, x, y, w, h, overhangWidth):
+def whiteHouse(t, x, y, w, h, overhang):
     lib.move.move(t, x, y)
     t.goto(x+w, y)
-    lib.move.move(t, x-overhangWidth, y+h)
-    t.goto(-37.5, 50)
-    t.goto(-50, 62.5)
-    t.goto(-100, 62.5)
-    t.goto(-112.5, 50)
+    lib.move.move(t, x-overhang, y+h)
+    t.goto(x+w+overhang, y+h)
+    t.goto(x+w, y+h+overhang)
+    t.goto(x, y+h+overhang)
+    t.goto(x-overhang, y+h)
